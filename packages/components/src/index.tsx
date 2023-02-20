43b06146b2f21935612da13122f4662d9b4cb7a1
xinxu <azzlzzxz@gmail.com>
2023-02-20 21:06:27 +0800

    feat: 更改

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -2,13 +2,13 @@
  * @Author: xinxu
  * @Date: 2023-02-12 15:41:09
  * @LastEditors: xinxu
- * @LastEditTime: 2023-02-19 14:45:29
+ * @LastEditTime: 2023-02-20 21:06:15
  * @FilePath: /DDLL-Components/packages/components/src/index.tsx
  */
 import React from 'react';
 
 const Component = () => {
-  return <div>Component</div>;
+  return <div>Componentssss</div>;
 };
 
 export default Component;
